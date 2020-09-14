6127cc3ac00f707a5c2f53621eed6d65b38cee86
Timi-of-Earth <shodiyatimilehin@gmail.com>
2020-09-14 01:02:00 +0100

    Added conversion functions for all length unit pairs, some stling

--- script.js
+++ script.js
@@ -11,23 +11,68 @@ var data = {
                 answer = arg*3.281;
                 return answer;
             },
-            yards: {},
-            miles: {}
+            yards: function metreToYards(arg) {
+                answer = arg * 1.094;
+                return answer;
+            },
+            miles: function metreToMiles(arg) {
+                answer = arg / 1609;
+                return answer;
+            },
+            metre: function metreToMetre(arg) {
+                return arg;
+            }
         },
         feet: {
-            metre: {},
-            yards: {},
-            miles: {}
+            metre: function feetToMetre(arg) {
+                answer = arg / 3.281;
+                return answer;
+            },
+            yards: function feetToYards(arg) {
+                answer = arg / 3;
+                return answer;
+            },
+            miles: function feetToMiles(arg) {
+                answer = arg / 5280;
+                return answer;
+            },
+            feet: function feetToFeet(arg) {
+                return arg;
+            }
         },
         yards: {
-            metre: {},
-            feet: {},
-            miles: {}
+            metre: function yardsToMetre(arg) {
+                answer = arg / 1.094;
+                return answer;
+            },
+            feet: function yardsToFeet(arg) {
+                answer = arg * 3;
+                return answer;
+            },
+            miles: function yardsToMiles(arg) {
+                answer = arg / 1760;
+                return answer;
+            },
+            yards: function yardsToYards(arg) {
+                return arg;
+            }
         },
         miles: {
-            metre: {},
-            feet: {},
-            yards: {}
+            metre: function milesToMetre(arg) {
+                answer = arg * 1609;
+                return answer;
+            },
+            feet: function milesToFeet(arg) {
+                answer =arg * 5280;
+                return answer;
+            },
+            yards: function milesToYards(arg) {
+                answer = arg * 1760;
+                return answer;
+            },
+            miles: function milesToMiles(arg) {
+                return arg;
+            }
         }
     },
     pressure: {},
@@ -40,6 +85,9 @@ const outputField = document.getElementById("num2");
 
 //function to initiate conversion of number in input field
 function convert() {
-    outputField.placeholder = data[quantity.value][fromUnit.value][toUnit.value](num1.value);
+    outputField.value = data[quantity.value][fromUnit.value][toUnit.value](num1.value);
     
+}
+
+function unitSet() {
 }
